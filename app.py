b782df889a9ebb805450fd5254fc631ddb0ab356
92an <micke92an@comhem.se>
2020-12-04 09:13:02 +0000

    registration functionality enabled

--- app.py
+++ app.py
@@ -57,6 +57,28 @@ def register():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        # check first if there is an existing user with that username
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # Make a check against the hased password
+            if check_password_hash(
+               existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Wlcome, {} to EconHub".format(
+                        request.form.get("username")))
+
+            else:
+                # Case if invalid password
+                flash("Incorrect username and/or password provided")
+                return redirect(url_for("login"))
+
+        else:
+            # Case if username is invalid
+            flash("Incorrect username and/or password provided")
+            return redirect(url_for("login"))
     return render_template("login.html")
 
 
