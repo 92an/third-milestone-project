8f720da074f7cd09c486690ad55a16a692445148
92an <micke92an@comhem.se>
2020-12-04 14:30:14 +0000

    added logout and profle to navbar, with dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,19 @@
   $(document).ready(function(){
+    // sidenav functionality materialize
     $(".sidenav").sidenav({edge: "right"});
+
+    // Carousel functionality materialize
     $('.carousel').carousel({
         numVisible: 1,
         fullWidth: true,
         indicators: true
     });
+
         // function that will automate the carousel
         setInterval(function(){
             $('.carousel').carousel('next');
         }, 12000);
+
+    // Dropdown functionality materialize
+    $(".dropdown-trigger").dropdown();
   });
