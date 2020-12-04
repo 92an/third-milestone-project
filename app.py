90a41be33f568f8b04baa55c2cd29e1e173cb55d
92an <micke92an@comhem.se>
2020-12-04 15:16:47 +0000

    added logout functionality to button and redirect to login page

--- app.py
+++ app.py
@@ -84,11 +84,24 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/logout")
+def logout():
+    # remove session cookie
+    flash("You have been logged out!")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
+
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+
+    if session["user"]:
+        return render_template("profile.html", username=username)
+
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
