794f043c1f3ab644d737b71bc50f27f8d1792881
92an <micke92an@comhem.se>
2020-12-16 12:05:27 +0000

    turned off debugger

--- app.py
+++ app.py
@@ -398,4 +398,4 @@ def student_chatt():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
